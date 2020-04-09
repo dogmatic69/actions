ff75c3849db0a70070e4eb42954ea71ae6a3d81a
Carl Sutton (dogmatic69) <dogmatic69@gmail.com>
2020-04-10 00:37:15 +0200

    feat: refactor PR automation
    create new action for toggling PRs using
    the github API

---
+++ automations/pr-toggle/files/work/toggle-pull-request.js
@@ -0,0 +1,31 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+module.exports = () => {
+  const token = process.env["GITHUB_TOKEN"] || "";
+  if (token === "") {
+    return core.setFailed("No token provided")
+  }
+  const pr = process.env["PULL_REQUEST_NUMBER"] || "";
+  if (pr === "") {
+    return core.setFailed("No PR number provided")
+  }
+
+  const client = new github.GitHub(token);
+
+  core.info('Updating the state of a pull request to [closed]');
+  await client.pulls.update({
+    ...github.context.repo,
+    pull_number: pr,
+    state: 'closed'
+  });
+
+  core.info('Updating the state of a pull request to [open]');
+  await client.pulls.update({
+    ...github.context.repo,
+    pull_number: pr,
+    state: 'open'
+  });
+
+  core.info(`Manipulated the PR [${pr}]`);
+};
