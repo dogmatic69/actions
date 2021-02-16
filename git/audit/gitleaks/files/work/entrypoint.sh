665981ddb63d4f9fa85c608eb7b55c4e168ef582
Carl Sutton <carl.sutton@ingka.ikea.com>
2021-02-16 10:01:19 +0100

    feat: add depth option
    Allow scanning a specific depth rather than the
    entire repository which slowly grows and takes
    longer time

--- git/audit/gitleaks/files/work/entrypoint.sh
+++ git/audit/gitleaks/files/work/entrypoint.sh
@@ -3,18 +3,21 @@ set -e
 
 REPO=$(realpath ${INPUT_PATH})
 OUTPUT=/output/gitleaks.json
+DEPTH=${INPUT_DEPTH}
 
 #==============================================================================
-function finish {
+
+finish() {
   cat ${OUTPUT}
 }
+
 trap finish EXIT
 
 mkdir -p "$(dirname ${OUTPUT})"
 touch "${OUTPUT}"
 
-if [ -f $(git -C ${REPO} rev-parse --git-dir)/shallow ]; then
+if [ -f "$(git -C ${REPO} rev-parse --git-dir)/shallow" ]; then
     echo "{\"error\": \"Shallow clone detected. fetch-depth must be set to 0\"}" > ${OUTPUT}
 fi
 
-gitleaks -v --redact --repo-path=${REPO} --report=${OUTPUT}
+gitleaks -v --redact --repo-path="${REPO}" --report=${OUTPUT} --depth="${DEPTH}"
