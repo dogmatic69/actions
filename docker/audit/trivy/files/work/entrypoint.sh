83879ac1a164c37c9b0cd13ea326315d9060bdb7
Carl Sutton (dogmatic69) <dogmatic69@gmail.com>
2026-02-13 21:54:08 +0100

    fix: trivy file issues

--- docker/audit/trivy/files/work/entrypoint.sh
+++ docker/audit/trivy/files/work/entrypoint.sh
@@ -3,13 +3,16 @@
 echo "Running trivy scan: [${INPUT_IMAGE}]"
 trivy clean --scan-cache
 
+OUTPUT="${INPUT_OUTPUT:-/output/trivy.json}"
+mkdir -p "$(dirname "${OUTPUT}")"
+
 IGNOREFILE_FLAG=""
-if [ -f "/.trivyignore" ]; then
-	IGNOREFILE_FLAG="--ignorefile /.trivyignore"
+if [ -f "${INPUT_IGNORE:-/.trivyignore}" ]; then
+	IGNOREFILE_FLAG="--ignorefile ${INPUT_IGNORE:-/.trivyignore}"
 fi
 
 GITHUB_TOKEN=${INPUT_TOKEN} \
 trivy image --no-progress --exit-code 1 \
-	-f json -o /output/trivy.json \
+	-f json -o "${OUTPUT}" \
 	${IGNOREFILE_FLAG} \
 	"${INPUT_IMAGE}"
