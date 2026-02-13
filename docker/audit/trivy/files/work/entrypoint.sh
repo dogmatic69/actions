43c8b1435916634970581ca284824064eb733018
Carl Sutton (dogmatic69) <dogmatic69@gmail.com>
2026-02-13 16:23:54 +0100

    fix: handle missing ignore in trivy

--- docker/audit/trivy/files/work/entrypoint.sh
+++ docker/audit/trivy/files/work/entrypoint.sh
@@ -2,8 +2,14 @@
 
 echo "Running trivy scan: [${INPUT_IMAGE}]"
 trivy clean --scan-cache
+
+IGNOREFILE_FLAG=""
+if [ -f "/.trivyignore" ]; then
+	IGNOREFILE_FLAG="--ignorefile /.trivyignore"
+fi
+
 GITHUB_TOKEN=${INPUT_TOKEN} \
 trivy image --no-progress --exit-code 1 \
 	-f json -o /output/trivy.json \
-	--ignorefile /.trivyignore \
+	${IGNOREFILE_FLAG} \
 	"${INPUT_IMAGE}"
