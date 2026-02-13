ebfb08bbd1f91883b41d222aa02a9706f446e5fc
Carl Sutton (dogmatic69) <dogmatic69@gmail.com>
2026-02-13 16:15:35 +0100

    fix: update eslint

---
+++ src/configurator/files/src/eslint.config.mjs
@@ -0,0 +1,19 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+export default [
+  {
+    ignores: ["node_modules/", "eslint.config.mjs"],
+  },
+  ...compat.extends("airbnb"),
+];
