f5e924878926f560abc2cbcf444cc15ccc194481
Carl Sutton (dogmatic69) <dogmatic69@gmail.com>
2026-02-14 20:39:50 +0100

    fix: disable unfixable errors with option

--- docker/audit/trivy/files/work/entrypoint.sh
+++ docker/audit/trivy/files/work/entrypoint.sh
@@ -11,9 +11,15 @@ if [ -f "${INPUT_IGNORE:-/.trivyignore}" ]; then
 	IGNOREFILE_FLAG="--ignorefile ${INPUT_IGNORE:-/.trivyignore}"
 fi
 
+IGNORE_UNFIXED_FLAG=""
+if [ "${INPUT_IGNORE_UNFIXED:-true}" = "true" ]; then
+	IGNORE_UNFIXED_FLAG="--ignore-unfixed"
+fi
+
 GITHUB_TOKEN=${INPUT_TOKEN} \
 trivy image --no-progress \
 	--scanners vuln \
+	${IGNORE_UNFIXED_FLAG} \
 	--exit-code 1 \
 	-f json -o "${OUTPUT}" \
 	${IGNOREFILE_FLAG} \
@@ -24,6 +30,7 @@ SCAN_EXIT=$?
 # Print results to the console
 trivy image --no-progress \
 	--scanners vuln \
+	${IGNORE_UNFIXED_FLAG} \
 	-f table \
 	${IGNOREFILE_FLAG} \
 	--skip-db-update \
